f8e49ea699bd0a9688dad658afc009c2a6253489
Bella Norvig <bellbell@gmail.com>
2013-08-21 18:20:27 -0400

    files for deploying on Heroku

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express')
+  , http    = require('http');
+  
+  var app = express();
+
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/public');
+  app.use(express.static('public'));
+
+  app.get('/', function(req, res) {
+    res.sendfile(__dirname+'/public/boxes_html.html');
+  });
+
+  http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port') + '.');
+  });
+  
